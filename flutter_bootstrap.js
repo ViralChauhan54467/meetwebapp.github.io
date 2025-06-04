cfd804eb89a0e2686fac83c0eba1a351054586cb
ViralChauhan54467 <ViralChauhan54467@users.noreply.github.com>
2025-06-04 06:55:48 +0000

    deploy: 72244231cdc31c292613270bb5e7e74afe9ceb99

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3844945152"
+    serviceWorkerVersion: "167388003"
   }
 });
